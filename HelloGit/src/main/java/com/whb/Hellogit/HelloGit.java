e7002cf31982fd83016627b1eaafde477810f5de
JINWANDALAOHU250 <2509037018@qq.com>
2021-09-15 12:09:19 +0800

    更新1

--- HelloGit/src/main/java/com/whb/Hellogit/HelloGit.java
+++ HelloGit/src/main/java/com/whb/Hellogit/HelloGit.java
@@ -3,5 +3,6 @@ package com.whb.Hellogit;
 public class HelloGit {
     public static void main(String[] args) {
         System.out.println("hellogit");
+        System.out.println("跟新1");
     }
 }
